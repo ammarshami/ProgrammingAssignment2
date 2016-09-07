5212a1fdaef4b3a067861b06d3305164c2a4b769
ammarshami <ammarshami@hotmail.com>
2016-09-07 09:24:04 +0300

    assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,43 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
+## This function has one parameter of type matrix, it creates a cache of a matrix inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL
+  set <- function(y) {
+    x <<- y
+    inverse <<- NULL
+  }
+  get <- function() x
+  setInverse <- function(inverse) inv <<- inverse
+  getInverse <- function() inv
+  list(set = set, get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
 }
 
+## This function computes the inverse of the "matrix" returned by makeCacheMatrix above. If the inverse has already.
+## been calculated (and the matrix has not changed), then cacheSolve will retrieve the inverse from the cache. The function
+## assumes that the matrix ihas an inverse, then the new result is cached
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x) {
+  ## Return the inverse matrix of x
+  inv <- x$getInverse()
+  ## check if the inverse exists
+  if(!is.null(inv)) {
+    ## the inverse exists, return it
+    return(inv)
+  } else {
+    ## inverse does not exist, compute it and return it
+    matInv <- x$get()
+    inv <- solve(matInv)
+    ## cache the new inverse
+    x$setInverse(inv)
+    inv
+  }
 }
+
+## Usage example:
+## x <- matrix(1:4 , ncol=2, nrow =2)
+## mat <- makeCacheMatrix(x) 
+## cacheSolve(mat)
+
